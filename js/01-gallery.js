c5eff89ed9eb2499f97075be0d35a2052be1d050
Hanna1991-sys <119236984+Hanna1991-sys@users.noreply.github.com>
2023-10-27 15:51:53 +0300

    end

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,35 +1,42 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 const list = document.querySelector(".gallery");
 
-
 list.insertAdjacentHTML("beforeend", createMarkup(galleryItems));
 
-galleryContainer.addEventListener("click", handleClick);
-
-function handleClick(event) {
-  if (event.target == event.currentTarget) {
-    return;
-  }
-  const currentImg = event.target.closest(".gallery__item");
-}
+list.addEventListener("click", handleClick);
 
 function createMarkup(items) {
-    return items
-      .map(({ preview, original, description }) => `
-        <li class="gallery__item">
-  <a class="gallery__link" href="large-image.jpg">
-    <img
-      class="gallery__image"
-      src="${preview}"
-      data-source="${original}"
-      alt="${description}"
-    />
-  </a>
-</li>
-      `).join("");
+  return items
+    .map(
+      ({ preview, original, description }) => `
+               <li class="gallery__item">
+                <a class="gallery__link" href="large-image.jpg">
+                  <img
+                  class="gallery__image"
+                  src="${preview}"
+                  data-source="${original}"
+                  alt="${description}"
+                  />
+                </a>
+               </li>
+      `
+    )
+    .join("");
 }
 
+const instance = basicLightbox.create(`
+    <img width="1280" height="auto" src="">
+`);
 
- 
+function handleClick(event) {
+  event.preventDefault();
+  const datasetSource = event.target.dataset.source;
+  if (!datasetSource) {
+    return;
+  }
+  instance.element().querySelector("img").src = datasetSource;
+
+  instance.show();
+}
